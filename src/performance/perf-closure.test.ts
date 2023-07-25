a1cd76e6ed36479d8743118cd228ead3efac3bde
Marco Greco <marcoandreagreco@gmail.com>
2023-07-25 11:38:54 +0200

    fix type in src/performance/perf-closure.test.ts
    Co-authored-by: Fernando-Granato <117268775+Fernando-Granato@users.noreply.github.com>

--- src/performance/perf-closure.test.ts
+++ src/performance/perf-closure.test.ts
@@ -52,7 +52,7 @@ export default function () {
     });
     check(
       responseClosure,
-        { "Response status from POST newDebtPosition was 201": (r) => r.status == 200 },
+        { "Response status from POST newDebtPosition was 200": (r) => r.status == 200 },
         { name: "closeDebtPosition" }
     );
 }
